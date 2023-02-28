b4f8c0b14ad2aeba94a909371d1d40ab2747d129
egiahmadyu <a.rizkyprayogi@gmail.com>
2023-03-01 06:31:12 +0700

    1

--- resources/views/pages/data_pelanggaran/proses/diterima.blade.php
+++ resources/views/pages/data_pelanggaran/proses/diterima.blade.php
@@ -7,7 +7,8 @@
             <div>
 
                 @if ($kasus->status_id > 1)
-                    <button type="button" class="btn btn-primary" onclick="getViewProcess(3)">Selanjutnya <i class="far fa-arrow-right"></i></button>
+                    <button type="button" class="btn btn-primary" onclick="getViewProcess(3)">Selanjutnya <i
+                            class="far fa-arrow-right"></i></button>
                 @endif
 
             </div>
@@ -49,19 +50,23 @@
             <div class="row">
                 <div class="col-lg-6 mb-3">
                     <label for="no_nota_dinas" class="form-label">No. Nota Dinas</label>
-                    <input type="text" name="no_nota_dinas" class="form-control border-dark" placeholder="No. Nota Dinas" value="{{ isset($kasus) ? $kasus->no_nota_dinas : '' }}" >
+                    <input type="text" name="no_nota_dinas" class="form-control border-dark"
+                        placeholder="No. Nota Dinas" value="{{ isset($kasus) ? $kasus->no_nota_dinas : '' }}">
                 </div>
                 <div class="col-lg-6 mb-3">
                     <label for="perihal_nota_dinas" class="form-label">Perihal Nota Dinas</label>
-                    <input type="text" name="perihal_nota_dinas" class="form-control border-dark" placeholder="Perihal Nota Dinas" value="{{ isset($kasus) ? $kasus->perihal_nota_dinas : '' }}" >
+                    <input type="text" name="perihal_nota_dinas" class="form-control border-dark"
+                        placeholder="Perihal Nota Dinas" value="{{ isset($kasus) ? $kasus->perihal_nota_dinas : '' }}">
                 </div>
                 <div class="col-lg-6 mb-3">
                     <label for="wujud_perbuatan" class="form-label">Wujud Perbuatan</label>
-                    <input type="text" name="wujud_perbuatan" class="form-control border-dark" placeholder="Wujud Perbuatan" value="{{ isset($kasus) ? $kasus->wujud_perbuatan : '' }}" >
+                    <input type="text" name="wujud_perbuatan" class="form-control border-dark"
+                        placeholder="Wujud Perbuatan" value="{{ isset($kasus) ? $kasus->wujud_perbuatan : '' }}">
                 </div>
                 <div class="col-lg-6 mb-3">
                     <label for="tanggal_nota_dinas" class="form-label">Tanggal Nota Dinas</label>
-                    <input type="text" name="tanggal_nota_dinas" class="form-control border-dark" placeholder="Tanggal Nota Dinas" value="{{ isset($kasus) ? $kasus->tanggal_nota_dinas : '' }}" >
+                    <input type="text" name="tanggal_nota_dinas" class="form-control border-dark"
+                        placeholder="Tanggal Nota Dinas" value="{{ isset($kasus) ? $kasus->tanggal_nota_dinas : '' }}">
                 </div>
                 <hr>
             </div>
@@ -70,102 +75,123 @@
                     <div class="row">
                         <div class="col-lg-12 mb-3">
                             <label for="pelapor" class="form-label">Pelapor</label>
-                            <input type="text" name="pelapor" class="form-control border-dark" value="{{ isset($kasus) ? $kasus->pelapor : '' }}" >
+                            <input type="text" name="pelapor" class="form-control border-dark"
+                                value="{{ isset($kasus) ? $kasus->pelapor : '' }}">
                         </div>
                         <div class="col-lg-6 mb-3">
                             <label for="umur" class="form-label">Umur</label>
-                            <input type="number" name="umur" class="form-control border-dark" value="{{ isset($kasus) ? $kasus->umur : '' }}" >
+                            <input type="number" name="umur" class="form-control border-dark"
+                                value="{{ isset($kasus) ? $kasus->umur : '' }}">
                         </div>
                         <div class="col-lg-6 mb-3">
                             <label for="jenis_kelamin" class="form-label">Jenis Kelamin</label>
-                            <select class="form-select border-dark" aria-label="Default select example" name="jenis_kelamin" id="jenis_kelamin">
+                            <select class="form-select border-dark" aria-label="Default select example"
+                                name="jenis_kelamin" id="jenis_kelamin">
                                 <option value="">-- Jenis Kelamin --</option>
                                 @if (isset($jenis_kelamin))
                                     @foreach ($jenis_kelamin as $key => $jk)
-                                        <option value="{{ $jk->id }}" {{ $kasus->jenis_identitas == $jk->id ? 'selected' : '' }}>{{ $jk->name }}</option>
+                                        <option value="{{ $jk->id }}"
+                                            {{ $kasus->jenis_identitas == $jk->id ? 'selected' : '' }}>
+                                            {{ $jk->name }}</option>
                                     @endforeach
                                 @endif
                             </select>
                         </div>
                         <div class="col-lg-6 mb-3">
                             <label for="pekerjaan" class="form-label">Pekerjaan</label>
-                            <input type="text" name="pekerjaan" class="form-control border-dark" value="{{ isset($kasus) ? $kasus->pekerjaan : '' }}" >
+                            <input type="text" name="pekerjaan" class="form-control border-dark"
+                                value="{{ isset($kasus) ? $kasus->pekerjaan : '' }}">
                         </div>
                         <div class="col-lg-6 mb-3">
                             <label for="agama" class="form-label">Agama</label>
                             {{-- <input type="text" name="agama" class="form-control" value="{{ isset($kasus) ? ($kasus->agama == 0 ? 'Islam' : 'Kristen') : '' }}" > --}}
-                            <select class="form-select border-dark" aria-label="Default select example" name="agama" id="agama">
+                            <select class="form-select border-dark" aria-label="Default select example"
+                                name="agama" id="agama" required>
                                 <option value="">-- Agama --</option>
                                 @if (isset($agama))
                                     @foreach ($agama as $key => $ag)
-                                        <option value="{{ $ag->id }}" {{ $kasus->agama == $ag->id ? 'selected' : '' }}>{{ $ag->name }}</option>
+                                        <option value="{{ $ag->id }}"
+                                            {{ $kasus->agama == $ag->id ? 'selected' : '' }}>{{ $ag->name }}
+                                        </option>
                                     @endforeach
                                 @endif
                             </select>
                         </div>
                         <div class="col-lg-6 mb-3">
                             <label for="no_identitas" class="form-label">No Identitas</label>
-                            <input type="text" name="no_identitas" class="form-control border-dark" value="{{ isset($kasus) ? $kasus->no_identitas : ''}}" >
+                            <input type="text" name="no_identitas" class="form-control border-dark"
+                                value="{{ isset($kasus) ? $kasus->no_identitas : '' }}" required>
                         </div>
                         <div class="col-lg-6 mb-3">
                             <label for="jenis_identitas" class="form-label">Jenis Identitas</label>
                             {{-- <input type="text" name="jenis_identitas" class="form-control" value="{{ isset($kasus) ? $kasus->jenis_identitas : '' }}" > --}}
-                            <select class="form-select border-dark" aria-label="Default select example" name="jenis_identitas" id="jenis_identitas">
+                            <select class="form-select border-dark" aria-label="Default select example"
+                                name="jenis_identitas" id="jenis_identitas">
                                 <option value="">-- Jenis Identitas --</option>
                                 @if (isset($jenis_identitas))
                                     @foreach ($jenis_identitas as $key => $ji)
-                                        <option value="{{ $ji->id }}" {{ $kasus->jenis_identitas == $ji->id ? 'selected' : '' }}>{{ $ji->name }}</option>
+                                        <option value="{{ $ji->id }}"
+                                            {{ $kasus->jenis_identitas == $ji->id ? 'selected' : '' }}>
+                                            {{ $ji->name }}</option>
                                     @endforeach
                                 @endif
                             </select>
                         </div>
                         <div class="col-lg-12 mb-3">
                             <label for="alamat" class="form-label">Alamat</label>
-                            <textarea name="alamat" cols="30" rows="9" placeholder="Alamat" class="form-control border-dark" value="{{ isset($kasus) ? $kasus->alamat : '' }}">{{ isset($kasus) ? $kasus->alamat : '' }}</textarea>
+                            <textarea name="alamat" cols="30" rows="9" placeholder="Alamat" class="form-control border-dark"
+                                value="{{ isset($kasus) ? $kasus->alamat : '' }}">{{ isset($kasus) ? $kasus->alamat : '' }}</textarea>
                         </div>
                     </div>
                     <hr>
                 </div>
-                
+
                 <div class="col-lg-6">
                     <div class="row">
                         <div class="col-lg-12 mb-3">
                             <label for="terlapor" class="form-label">Terlapor</label>
-                            <input type="text" name="terlapor" class="form-control border-dark" value="{{ isset($kasus) ? $kasus->terlapor : '' }}" >
+                            <input type="text" name="terlapor" class="form-control border-dark"
+                                value="{{ isset($kasus) ? $kasus->terlapor : '' }}">
                         </div>
                         <div class="col-lg-6 mb-3">
                             <label for="pangkat" class="form-label">Pangkat</label>
-                            <input type="text" name="pangkat" class="form-control border-dark" value="{{ isset($kasus) ? $kasus->pangkat : '' }}" >
+                            <input type="text" name="pangkat" class="form-control border-dark"
+                                value="{{ isset($kasus) ? $kasus->pangkat : '' }}">
                         </div>
                         <div class="col-lg-6 mb-3">
                             <label for="nrp" class="form-label">NRP</label>
-                            <input type="text" name="nrp" placeholder="NRP Terlapor" class="form-control border-dark" value="{{ isset($kasus) ? $kasus->nrp : '' }}" >
+                            <input type="text" name="nrp" placeholder="NRP Terlapor"
+                                class="form-control border-dark" value="{{ isset($kasus) ? $kasus->nrp : '' }}">
                         </div>
                         <div class="col-lg-6 mb-3">
                             <label for="jabatan" class="form-label">Jabatan</label>
-                            <input type="text" name="jabatan" placeholder="Jabatan Terlapor" class="form-control border-dark" value="{{ isset($kasus) ? $kasus->jabatan : '' }}" >
+                            <input type="text" name="jabatan" placeholder="Jabatan Terlapor"
+                                class="form-control border-dark" value="{{ isset($kasus) ? $kasus->jabatan : '' }}">
                         </div>
                         <div class="col-lg-6 mb-3">
                             <label for="kesatuan" class="form-label">Kesatuan</label>
-                            <input type="text" name="kesatuan" class="form-control border-dark" value="{{ isset($kasus) ? $kasus->kesatuan : '' }}" >
+                            <input type="text" name="kesatuan" class="form-control border-dark"
+                                value="{{ isset($kasus) ? $kasus->kesatuan : '' }}">
                         </div>
                         <div class="col-lg-6 mb-3">
                             <label for="tempat_kejadian" class="form-label">Tempat Kejadian</label>
-                            <input type="text" name="tempat_kejadian" class="form-control border-dark" value="{{ isset($kasus) ? $kasus->tempat_kejadian : '' }}"
-                                >
+                            <input type="text" name="tempat_kejadian" class="form-control border-dark"
+                                value="{{ isset($kasus) ? $kasus->tempat_kejadian : '' }}">
                         </div>
                         <div class="col-lg-6 mb-3">
                             <label for="tanggal_kejadian" class="form-label">Tanggal Kejadian</label>
-                            <input type="text" name="tanggal_kejadian" class="form-control border-dark" value="{{ isset($kasus) ? $kasus->tanggal_kejadian : '' }}"
-                                >
+                            <input type="text" name="tanggal_kejadian" class="form-control border-dark"
+                                value="{{ isset($kasus) ? $kasus->tanggal_kejadian : '' }}">
                         </div>
                         <div class="col-lg-12 mb-3">
                             <label for="nama_korban" class="form-label">Nama Korban</label>
-                            <input type="text" name="nama_korban" class="form-control border-dark" value="{{ isset($kasus) ? $kasus->nama_korban : '' }}" >
+                            <input type="text" name="nama_korban" class="form-control border-dark"
+                                value="{{ isset($kasus) ? $kasus->nama_korban : '' }}">
                         </div>
                         <div class="col-lg-12 mb-3">
                             <label for="exampleFormControlInput1" class="form-label">Kronologis</label>
-                            <textarea name="kronologis" cols="30" rows="5" placeholder="Alamat" class="form-control border-dark" value="{{ isset($kasus) ? $kasus->kronologi : '' }}">{{ isset($kasus) ? $kasus->kronologi : '' }}</textarea>
+                            <textarea name="kronologis" cols="30" rows="5" placeholder="Alamat" class="form-control border-dark"
+                                value="{{ isset($kasus) ? $kasus->kronologi : '' }}">{{ isset($kasus) ? $kasus->kronologi : '' }}</textarea>
                         </div>
                     </div>
                     <hr>
@@ -175,7 +201,8 @@
                         <div class="col-lg-12 mb-3">
                             <div class="row">
                                 <div class="col-lg-4">
-                                    <label for="exampleFormControlInput1" class="form-label">Disposisi Karo/Sesro</label>
+                                    <label for="exampleFormControlInput1" class="form-label">Disposisi
+                                        Karo/Sesro</label>
                                     <button class="btn btn-primary" style="width: 100%" data-bs-toggle="modal"
                                         data-bs-target="#modal_disposisi" type="button">
                                         <i class="far fa-download"></i> Download
@@ -189,28 +216,32 @@
                                     </button>
                                 </div>
                                 <div class="col-lg-4">
-                                    <label for="exampleFormControlInput1" class="form-label">Disposisi Ka. Den A</label>
+                                    <label for="exampleFormControlInput1" class="form-label">Disposisi Ka. Den
+                                        A</label>
                                     <button class="btn btn-primary" style="width: 100%" data-bs-toggle="modal"
                                         data-bs-target="#modal_disposisi" type="button">
                                         <i class="far fa-download"></i> Download
                                     </button>
                                 </div>
                             </div>
-    
+
                             {{-- <input type="text" class="form-control" value="{{ $kasus->terlapor }}" > --}}
                         </div>
                         <div class="col-lg-12 mb-3">
                             <label for="exampleInputEmail1" class="form-label">Status</label>
-                            <select class="form-select border-dark" aria-label="Default select example" name="disposisi_tujuan"
-                                {{-- {{ 2 != $kasus->status_id ? 'disabled' : '' }}  --}} onchange="getPolda()" id="disposisi-tujuan">
+                            <select class="form-select border-dark" aria-label="Default select example"
+                                name="disposisi_tujuan" {{-- {{ 2 != $kasus->status_id ? 'disabled' : '' }}  --}} onchange="getPolda()"
+                                id="disposisi-tujuan">
                                 <option value="" class="text-center">-- Pilih Status --</option>
-                                <option value="4" class="text-center" {{ $kasus->status_id == 4 ? 'selected' : '' }}>Pulbaket</option>
-                                <option value="3" class="text-center" {{ $kasus->status_id == 3 ? 'selected' : '' }}>Limpah
+                                <option value="4" class="text-center"
+                                    {{ $kasus->status_id == 4 ? 'selected' : '' }}>Pulbaket</option>
+                                <option value="3" class="text-center"
+                                    {{ $kasus->status_id == 3 ? 'selected' : '' }}>Limpah
                                 </option>
                             </select>
                         </div>
                         <div class="col-lg-12 mb-3" id="limpah-polda">
-    
+
                         </div>
                     </div>
                 </div>
@@ -219,13 +250,14 @@
                 <div class="col-lg-6">
                     <div class="row">
                         <div class="col-6">
-                            <button class="btn btn-update-diterima btn-success" type="submit" value="update_data" name="type_submit">
+                            <button class="btn btn-update-diterima btn-success" type="submit" value="update_data"
+                                name="type_submit">
                                 <i class="far fa-upload"></i> Update Data
                             </button>
                         </div>
                         <div class="col-6">
-                            <button class="btn btn-update-diterima btn-primary" type="submit" value="update_status" name="type_submit"
-                                {{ $kasus->status_id > 1 ? 'disabled' : '' }}>
+                            <button class="btn btn-update-diterima btn-primary" type="submit" value="update_status"
+                                name="type_submit" {{ $kasus->status_id > 1 ? 'disabled' : '' }}>
                                 <i class="far fa-upload"></i> Update Status
                             </button>
                         </div>
