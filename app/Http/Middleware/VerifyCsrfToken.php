7af01d7067451b1b3ba735ad71894d7f24802f1b
egiahmadyu <a.rizkyprayogi@gmail.com>
2024-03-23 23:11:26 +0700

    ..

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -11,7 +11,26 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array<int, string>
      */
-    protected $except = [
-        //
-    ];
+    // protected $except = [
+    //     //
+    // ];
+
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $response->headers->setCookie(
+            new Cookie(
+                'XSRF-TOKEN',
+                $request->session()->token(),
+                time() + 60 * 120,
+                '/',
+                null,
+                true, // Set this to true for secure.
+                true, // Set this to true for httponly.
+                false,
+                'strict'
+            )
+        );
+        return $response;
+    }
 }
